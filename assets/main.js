03a98eb5848b45f739c7ae912d83627764b6e3e7
FrancescoDale <fd060862@gmail.com>
2020-11-24 16:27:10 +0100

    fix parentesi graffe nel main.js

--- assets/main.js
+++ assets/main.js
@@ -5,25 +5,21 @@ var app = new Vue ({
         moviesList : []
     },
     methods : {
-
         movieSrc() {
-            //controllo if per verificare che l'input non sia una stringa vuota
-            if(this.inputSearch != '') {
-                // console.log('funziona');
+            if (this.inputSearch != '') {
                 axios
-                //chiamata API + parametri
-                .get('https://api.themoviedb.org/3/movie',
-                    { params : {
-                        api_key : 'b2051a7083718dd2e6253bdfd616ec4e',
-                        query : this.inputSearch,
-                    }
-                })
+                .get('https://api.themoviedb.org/3/search/movie', { params : {
+                    api_key : 'b2051a7083718dd2e6253bdfd616ec4e',
+                    query : this.inputSearch,
+                } })
                 .then((response) => {
-                    console.log(response.data.response);
-                });
-
-            };
+                    this.moviesList = response.data.response;
+                })
+            }
         },
+
     },
+
+
     mounted() {},
 });
