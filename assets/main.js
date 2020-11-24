8dcf882e60d04d3e25430faf5d85fddd025f4ff9
FrancescoDale <fd060862@gmail.com>
2020-11-24 15:52:42 +0100

    ciclo v-for nell'index, chiamata API e identificazione dei parametri

--- assets/main.js
+++ assets/main.js
@@ -9,7 +9,11 @@ var app = new Vue ({
         movieSrc() {
             //controllo if per verificare che l'input non sia una stringa vuota
             if(this.inputSearch != '') {
-                console.log('funziona');
+                // console.log('funziona');
+                axios
+                //chiamata API + parametri
+                .get('https://api.themoviedb.org/3/movie', params : {})
+                .then
 
             };
         },
